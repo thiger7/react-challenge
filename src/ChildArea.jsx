8531d35f90c948453d6230c3ad2e0fe41b0d3ba3
adf0423 <adf0423@gmail.com>
2023-11-06 22:39:34 +0900

    useCallback, useMemoの使い方

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -7,7 +7,7 @@ const style = {
 };
 
 export const ChildArea = memo((props) => {
-  const { open } = props;
+  const { open, onClickClose } = props;
 
   const data = [...Array(20).keys()];
   console.log(data);
@@ -19,6 +19,7 @@ export const ChildArea = memo((props) => {
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
